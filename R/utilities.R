529015da076a423915a17f8126b213a86cb9ae51
Adam <cmattsson@bcgsc.ca>
2022-03-17 15:55:03 -0700

    formatting messy colours

--- R/utilities.R
+++ R/utilities.R
@@ -1010,6 +1010,7 @@ collate_curated_sv_results = function(sample_table){
   return(sample_table)
 }
 
+
 #' Annotate mutations with their copy number information.
 #'
 #' @param this_sample Sample ID of the sample you want to annotate.
@@ -1675,157 +1676,162 @@ get_gambl_colours = function(classification = "all",
   all_colours = list()
   everything = c()
   blood_cols=ggsci::get_ash("blood")
-  all_colours[["hmrn"]] <-c(
-    "BCL2-MYC" = "#52000F",
-    "BCL2"="#721F0F",
-    "SOCS1/SGK1"="#D66B1F",
-    "TET2/SGK1"="#C41230",
-    "MYD88"="#3B5FAC",
-    "NOTCH2"="#7F3293",
-    "NOTCH1" = "#55B55E",
-    "Other"="#ACADAF"
-  )
-  all_colours[["EBV"]] =  c("EBV-positive"="#7F055F",
-                            "EBV-negative"="#E5A4CB",
-                            "POS"="#7F055F",
-                            "NEG"="#E5A4CB")
-  all_colours[["BL"]] = c("M53-BL"="#A6CEE3",
-                                         "DLBCL-1"="#721F0F",
-                                         "IC-BL"="#45425A",
-                                         "DGG-BL"="#33A02C",
-                                         "DLBCL-2"="#FB9A99",
-                                         "DLBCL-3"="#C41230")
-  all_colours[["FL"]]=c(dFL="#99C1B9",cFL="#D16666",DLBCL="#479450")
-  all_colours[["lymphgen"]] = c(
-    "EZB-MYC" = "#52000F",
-    "EZB" = "#721F0F",
-    "EZB-COMP" = "#C7371A",
-    "ST2" = "#C41230",
-    "ST2-COMP" = "#EC3251",
-    "MCD" = "#3B5FAC",
-    "MCD-COMP" = "#6787CB",
-    "BN2" =  "#7F3293",
-    "BN2-COMP" = "#A949C1",
-    "N1" = "#55B55E",
-    "N1-COMP" = "#7FC787",
-    "A53" = "#5b6d8a",
-    "Other" = "#ACADAF",
-    "COMPOSITE" = "#ACADAF"
 
-  )
+  all_colours[["hmrn"]] = c("BCL2-MYC" = "#52000F",
+                            "BCL2" = "#721F0F",
+                            "SOCS1/SGK1" = "#D66B1F",
+                            "TET2/SGK1" = "#C41230",
+                            "MYD88" = "#3B5FAC",
+                            "NOTCH2" = "#7F3293",
+                            "NOTCH1" = "#55B55E",
+                            "Other" = "#ACADAF")
+
+  all_colours[["EBV"]] =  c("EBV-positive" = "#7F055F",
+                            "EBV-negative" = "#E5A4CB",
+                            "POS" = "#7F055F",
+                            "NEG" = "#E5A4CB")
+
+  all_colours[["BL"]] = c("M53-BL" = "#A6CEE3",
+                          "DLBCL-1" = "#721F0F",
+                          "IC-BL" = "#45425A",
+                          "DGG-BL" = "#33A02C",
+                          "DLBCL-2" = "#FB9A99",
+                          "DLBCL-3" = "#C41230")
+
+  all_colours[["FL"]] = c(dFL = "#99C1B9", cFL = "#D16666", DLBCL = "#479450")
+
+  all_colours[["lymphgen"]] = c("EZB-MYC" = "#52000F",
+                                "EZB" = "#721F0F",
+                                "EZB-COMP" = "#C7371A",
+                                "ST2" = "#C41230",
+                                "ST2-COMP" = "#EC3251",
+                                "MCD" = "#3B5FAC",
+                                "MCD-COMP" = "#6787CB",
+                                "BN2" =  "#7F3293",
+                                "BN2-COMP" = "#A949C1",
+                                "N1" = "#55B55E",
+                                "N1-COMP" = "#7FC787",
+                                "A53" = "#5b6d8a",
+                                "Other" = "#ACADAF",
+                                "COMPOSITE" = "#ACADAF")
+
   #all_colours[["coding_class"]] = c("Frame_Shift_Del","Frame_Shift_Ins",
   #                 "In_Frame_Del","In_Frame_Ins",
   #                 "Missense_Mutation","Nonsense_Mutation",
   #                 "Nonstop_Mutation","Splice_Region","Splice_Site",
   #                 "Targeted_Region","Translation_Start_Site")
-  all_colours[["mutation"]]=
-    c(
-        "Nonsense_Mutation"=unname(blood_cols["Red"]),
-        "Missense_Mutation"=unname(blood_cols["Green"]),
-        "Multi_Hit"=unname(blood_cols["Steel Blue"]),
-        "Frame_Shift_Ins" = unname(blood_cols["Magenta"]),
-        "Frame_Shift_Del" = unname(blood_cols["Magenta"]),
-        "In_Frame_Ins" = unname(blood_cols["Brown"]),
-        "In_Frame_Del" = unname(blood_cols["Brown"]),
-        "Nonstop_Mutation" = unname(blood_cols["Light Blue"]),
-        "Translation_Start_Site" = unname(blood_cols["Lavendar"]),
-        "Splice_Site" = unname(blood_cols["Orange"]),
-        "Splice_Region" = unname(blood_cols["Orange"]),
-        "3'UTR" = unname(blood_cols["Yellow"]))
 
-  all_colours[["pos_neg"]]=c(
-    "POS"="#c41230",
-    "NEG"="#E88873",
-    "PARTIAL"="#E88873",
-    "yes"="#c41230",
-    "no"="#E88873",
-    "YES"="#c41230",
-    "NO"="#E88873",
-    "FAIL"="#bdbdc1",
-    "positive"="#c41230",
-    "negative"="#E88873",
-    "fail"="#bdbdc1")
+  all_colours[["mutation"]] = c("Nonsense_Mutation" = unname(blood_cols["Red"]),
+                                "Missense_Mutation" = unname(blood_cols["Green"]),
+                                "Multi_Hit" = unname(blood_cols["Steel Blue"]),
+                                "Frame_Shift_Ins" = unname(blood_cols["Magenta"]),
+                                "Frame_Shift_Del" = unname(blood_cols["Magenta"]),
+                                "In_Frame_Ins" = unname(blood_cols["Brown"]),
+                                "In_Frame_Del" = unname(blood_cols["Brown"]),
+                                "Nonstop_Mutation" = unname(blood_cols["Light Blue"]),
+                                "Translation_Start_Site" = unname(blood_cols["Lavendar"]),
+                                "Splice_Site" = unname(blood_cols["Orange"]),
+                                "Splice_Region" = unname(blood_cols["Orange"]),
+                                "3'UTR" = unname(blood_cols["Yellow"]))
 
-  all_colours[["copy_number"]]=c(
-    "nLOH"="#E026D7",
-    "14"="#380015",
-    "15"="#380015",
-    "13"="#380015",
-    "12"="#380015",
-    "11"="#380015",
-    "10"="#380015",
-    "9"="#380015",
-    "8"="#380015",
-    "7"="#380015",
-    "6"="#380015",
-    "5"="#67001F",
-    "4"="#B2182B",
-    "3"="#D6604D",
-    "2"="#ede4c7",
-    "1"="#92C5DE",
-    "0"="#4393C3"
-  )
-  all_colours[["blood"]] = c(
-      "Red" = "#c41230", "Blue"="#115284","Green" = "#39b54b",
-      "Purple" = "#5c266c", "Orange"="#fe9003","Green" = "#046852",
-      "Lavendar" = "#8781bd", "Steel Blue"= "#455564",
-      "Light Blue" = "#2cace3", "Magenta" = "#e90c8b", "Mustard" = "#b76d29",
-      "LimeGreen" = "#a4bb87", "Brown" = "#5f3a17", "Gray" = "#bdbdc1",
-      "Yellow" = "#f9bd1f"
-  )
-  all_colours[["sex"]]=c(
-    "M"="#118AB2",
-    "Male"="#118AB2",
-    "male"="#118AB2",
-    "F"="#EF476F",
-    "Female"="#EF476F",
-    "female"="#EF476F")
-  all_colours[["clinical"]]=ggsci::get_ash("clinical")
-  all_colours[["pathology"]] = c(
-      "B-ALL"="#C1C64B",
-      "CLL"="#889BE5",
-      "MCL"="#F37A20",
-      "BL"="#926CAD",
-      "mBL"="#34C7F4",
-      "tFL"="#FF8595",
-      "DLBCL-BL-like"="#34C7F4",
-      "pre-HT"="#754F5B",
-      "PMBL"= "#227C9D",
-      "FL"="#EA8368",
-      "no-HT"="#EA8368",
-      "COMFL"="#8BBC98",
-      "COM"="#8BBC98",
-      "post-HT"="#479450",
-      "DLBCL"="#479450",
-      "denovo-DLBCL"="#479450",
-      "HGBL-NOS"="#294936",
-      "HGBL"="#294936",
-      "HGBL-DH/TH"="#7A1616",
-      "PBL" = "#E058C0",
-      "CNS" = "#E2EF60",
-      "THRLBCL" = "#A5F2B3",
-      "MM"="#CC9A42",
-      "SCBC"="#8c9c90",
-      "UNSPECIFIED"="#cfba7c",
-      "MZL"="#065A7F"
-  )
-  all_colours[["coo"]] = c(
-    "ABC" = "#05ACEF",
-    "UNCLASS" = "#05631E",
-    "Unclass" = "#05631E",
-    "U" = "#05631E",
-    "UNC" = "#05631E",
-    "GCB"= "#F58F20",
-    "DHITsig-"= "#F58F20",
-    "DHITsigNeg"= "#F58F20",
-    "DHITsig-IND" = "#003049",
-    "DHITsig+" = "#D62828",
-    "DHITsigPos" = "#D62828",
-    "NA" = "#ACADAF"
-  )
-  all_colours[["cohort"]] = c("Chapuy"="#8B0000",
-                  "Arthur"= "#8845A8",
-                  "Schmitz"= "#2C72B2")
+  all_colours[["pos_neg"]] = c("POS" = "#c41230",
+                               "NEG" = "#E88873",
+                               "PARTIAL" = "#E88873",
+                               "yes" = "#c41230",
+                               "no" = "#E88873",
+                               "YES" = "#c41230",
+                               "NO" = "#E88873",
+                               "FAIL" = "#bdbdc1",
+                               "positive" = "#c41230",
+                               "negative" = "#E88873",
+                               "fail" = "#bdbdc1")
+
+  all_colours[["copy_number"]] = c("nLOH"="#E026D7",
+                                   "14" = "#380015",
+                                   "15" = "#380015",
+                                   "13" = "#380015",
+                                   "12" = "#380015",
+                                   "11" = "#380015",
+                                   "10" = "#380015",
+                                   "9" = "#380015",
+                                   "8" = "#380015",
+                                   "7" = "#380015",
+                                   "6" = "#380015",
+                                   "5" = "#67001F",
+                                   "4" = "#B2182B",
+                                   "3" = "#D6604D",
+                                   "2" = "#ede4c7",
+                                   "1" = "#92C5DE",
+                                   "0" = "#4393C3")
+
+  all_colours[["blood"]] = c("Red" = "#c41230", 
+                             "Blue" = "#115284",
+                             "Green" = "#39b54b",
+                             "Purple" = "#5c266c", 
+                             "Orange" = "#fe9003",
+                             "Green" = "#046852",
+                             "Lavendar" = "#8781bd",
+                             "Steel Blue"= "#455564",
+                             "Light Blue" = "#2cace3",
+                             "Magenta" = "#e90c8b",
+                             "Mustard" = "#b76d29",
+                             "LimeGreen" = "#a4bb87",
+                             "Brown" = "#5f3a17",
+                             "Gray" = "#bdbdc1",
+                             "Yellow" = "#f9bd1f")
+
+  all_colours[["sex"]] = c("M" = "#118AB2",
+                           "Male" = "#118AB2",
+                           "male" = "#118AB2",
+                           "F" = "#EF476F",
+                           "Female" = "#EF476F",
+                           "female" = "#EF476F")
+
+  all_colours[["clinical"]] = ggsci::get_ash("clinical")
+
+  all_colours[["pathology"]] = c("B-ALL" = "#C1C64B",
+                                 "CLL" = "#889BE5",
+                                 "MCL" = "#F37A20",
+                                 "BL" = "#926CAD",
+                                 "mBL" = "#34C7F4",
+                                 "tFL" = "#FF8595",
+                                 "DLBCL-BL-like" = "#34C7F4",
+                                 "pre-HT" = "#754F5B",
+                                 "PMBL" = "#227C9D",
+                                 "FL" = "#EA8368",
+                                 "no-HT" = "#EA8368",
+                                 "COMFL" = "#8BBC98",
+                                 "COM" = "#8BBC98",
+                                 "post-HT" = "#479450",
+                                 "DLBCL" = "#479450",
+                                 "denovo-DLBCL" = "#479450",
+                                 "HGBL-NOS" = "#294936",
+                                 "HGBL" = "#294936",
+                                 "HGBL-DH/TH" = "#7A1616",
+                                 "PBL" = "#E058C0",
+                                 "CNS" = "#E2EF60",
+                                 "THRLBCL" = "#A5F2B3",
+                                 "MM" = "#CC9A42",
+                                 "SCBC" = "#8c9c90",
+                                 "UNSPECIFIED" = "#cfba7c",
+                                 "MZL" = "#065A7F")
+
+  all_colours[["coo"]] = c("ABC" = "#05ACEF",
+                           "UNCLASS" = "#05631E",
+                           "Unclass" = "#05631E",
+                           "U" = "#05631E",
+                           "UNC" = "#05631E",
+                           "GCB"= "#F58F20",
+                           "DHITsig-" = "#F58F20",
+                           "DHITsigNeg" = "#F58F20",
+                           "DHITsig-IND" = "#003049",
+                           "DHITsig+" = "#D62828",
+                           "DHITsigPos" = "#D62828",
+                           "NA" = "#ACADAF")
+
+  all_colours[["cohort"]] = c("Chapuy" = "#8B0000",
+                              "Arthur" = "#8845A8",
+                              "Schmitz" = "#2C72B2")
   
   #print(all_colours)
   for(colslot in names(all_colours)){
