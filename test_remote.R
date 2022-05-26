c0a144bad4a86d228f792667be10aa7947a34d1f
Ryan Morin <rdmorin@sfu.ca>
2022-05-26 10:19:00 -0700

    some fixes for remote useability with clone of gambl repo

---
+++ test_remote.R
@@ -0,0 +1,13 @@
+
+setwd("~/git/gambl/") # set this path to point to your local clone of the gambl repo
+# in your config.yml file set repo_base under the remote config (at the top) to point to this path also
+library(GAMBLR)
+Sys.setenv(R_CONFIG_ACTIVE= "remote")
+
+
+config::get("project_base")
+#[1] "/Users/rmorin/gambl_results/"
+
+all_meta = get_gambl_metadata()
+
+collated = collate_results(from_cache=TRUE)
