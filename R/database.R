1c0692ce141e6c80d8792cfb10c47babe95bffe5
Your Name <email@domain>
2022-06-24 16:19:12 -0400

    fix new bugs associated with ssh_session

--- R/database.R
+++ R/database.R
@@ -1739,7 +1739,7 @@ get_ssm_by_regions = function(regions_list,
                               seq_type = "genome",
                               projection = "grch37",
                               min_read_support = 4,
-                              ssh_session = FALSE){
+                              ssh_session){
 
   bed2region = function(x){
     paste0(x[1], ":", as.numeric(x[2]), "-", as.numeric(x[3]))
@@ -1828,7 +1828,7 @@ get_ssm_by_region = function(chromosome,
                              augmented = TRUE,
                              min_read_support = 3,
                              mode = "slms-3",
-                             ssh_session = FALSE){
+                             ssh_session ){
 
   tabix_bin = config::get("dependencies")$tabix
   table_name = config::get("results_tables")$ssm
@@ -1875,7 +1875,7 @@ get_ssm_by_region = function(chromosome,
 
   if(missing(maf_data)){
     if(from_indexed_flatfile){
-      if(ssh_session){
+      if(!missing(ssh_session)){
         #Helper function that may come in handy elsewhere so could be moved out of this function if necessary
         run_command_remote = function(ssh_session,to_run){
         output = ssh::ssh_exec_internal(ssh_session,to_run)$stdout
@@ -1890,8 +1890,9 @@ get_ssm_by_region = function(chromosome,
         message(paste("reading from:", full_maf_path_comp))
         tabix_command = paste(tabix_bin, full_maf_path_comp, region, "| cut -f 5,6,7,16,42")
         muts = run_command_remote(ssh_session,tabix_command)
+        print(tabix_command)
         muts_region = vroom::vroom(I(muts),col_types = "ciici",
-                                   col_names=c("Chromosome", "Start_Position", "End_Position", "Tumor_Sample_Barcode", "Read_Support"))
+                                   col_names=c("Chromosome", "Start_Position", "End_Position", "Tumor_Sample_Barcode", "t_alt_count"))
       }else{
         #get column names for maf and read maf
         maf_head = as.vector(as.matrix(read.table(file = full_maf_path, header = FALSE, stringsAsFactors = FALSE, nrows = 1)))
