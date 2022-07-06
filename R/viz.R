87a526d3c5ea87d9834398545db56092bb0c39d6
Adam <cmattsson@bcgsc.ca>
2022-07-06 16:32:02 -0700

    updates to fancy_proportions_plot

--- R/viz.R
+++ R/viz.R
@@ -4848,34 +4848,41 @@ fancy_proportions_plot = function(these_samples,
   qc_sub = dplyr::select(qc_metrics, sample_id, ProportionReadsDuplicated, ProportionReadsMapped, ProportionCoverage10x, ProportionCoverage30x) %>%
     gather(Type, Value, -sample_id)
 
+  qc_sub$Type = as.factor(qc_sub$Type)
+
+  levels(qc_sub$Type)[levels(qc_sub$Type)=="ProportionReadsDuplicated"] = "Duplicated Reads"
+  levels(qc_sub$Type)[levels(qc_sub$Type)=="ProportionReadsMapped"] = "Mapped Reads"
+  levels(qc_sub$Type)[levels(qc_sub$Type)=="ProportionCoverage10x"] = "10X"
+  levels(qc_sub$Type)[levels(qc_sub$Type)=="ProportionCoverage30x"] = "30X"
+
   #get means for each metric
-  mean_df = data.frame(Metric = c("ProportionReadsDuplicated", "ProportionReadsMapped", "ProportionCoverage10x", "ProportionCoverage30x"),
-                       Value = c(mean(qc_sub$Value[qc_sub$Type == "ProportionReadsDuplicated"]),
-                                 mean(qc_sub$Value[qc_sub$Type == "ProportionReadsMapped"]),
-                                 mean(qc_sub$Value[qc_sub$Type == "ProportionCoverage10x"]),
-                                 mean(qc_sub$Value[qc_sub$Type == "ProportionCoverage30x"])))
+  mean_df = data.frame(Metric = c("Duplicated Reads", "Mapped Reads", "10X", "30X"),
+                       Value = c(mean(qc_sub$Value[qc_sub$Type == "Duplicated Reads"]),
+                                 mean(qc_sub$Value[qc_sub$Type == "Mapped Reads"]),
+                                 mean(qc_sub$Value[qc_sub$Type == "10X"]),
+                                 mean(qc_sub$Value[qc_sub$Type == "30X"])))
 
   #set colors
-  these_colors = c("ProportionReadsDuplicated" = "#B8794D",
-                   "ProportionReadsMapped" = "#366B32",
-                   "ProportionCoverage10x" = "#DDA15E",
-                   "ProportionCoverage30x" = "#433D6B")
+  these_colors = c("Duplicated Reads" = "#B8794D",
+                   "Mapped Reads" = "#366B32",
+                   "10X" = "#DDA15E",
+                   "30X" = "#433D6B")
 
   #plotting
-  plot = ggplot(qc_sub, aes(x = sample_id, y = Value, group = Type)) +
-    geom_bar(aes(fill = Type), position = "dodge", stat = "identity") +
-    geom_hline(mean_df, mapping = aes(yintercept = Value, color = Metric), size = 0.5, linetype = "solid") +
+  p = ggplot(qc_sub, aes(x = sample_id, y = Value, group = Type)) +
+    geom_bar(aes(fill = Type), position = "dodge", stat = "identity", width = 0.7) +
+    geom_hline(mean_df, mapping = aes(yintercept = Value, color = Metric), size = 0.5, linetype = "dashed") +
     labs(title = "QC Metrics As Proportions (of All Reads)", subtitle = plot_subtitle, x = "", y = "Proportion") +
-    scale_y_continuous(breaks = seq(0, 1, by = 0.1)) +
+    scale_y_continuous(expand = c(0, 0), breaks = seq(0, 1, by = 0.1)) +
     scale_fill_manual(values = these_colors) +
     scale_color_manual(values = these_colors) +
-    theme(legend.position = "right", legend.title = element_blank(), axis.title.x = element_blank(),
-          axis.text.x = element_text(angle = 90, vjust = 0.5), axis.ticks.x = element_blank(),
-          panel.grid.major.x = element_line(color = "#454A4D", size = 0.5, linetype = 1),
-          panel.grid.minor.x = element_line(color = "#454A4D", size = 0.5, linetype = 1),
-          panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(),
-          axis.line.x = element_line(color = "black", size = 1, linetype = 1),
-          axis.line.y = element_line(color = "black", size = 1, linetype = 1),panel.background = element_blank())
+    labs(fill = "Metrics", color = "Mean") +
+    theme(legend.position = "right", axis.title.x = element_blank(), axis.text.x = element_text(angle = 90, vjust = 0.5),
+          axis.ticks.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(),
+          panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(), panel.background = element_blank(),
+          axis.line.x = element_line(color = "black", size = 0.5, linetype = 1),
+          axis.line.y = element_line(color = "black", size = 0.5, linetype = 1),
+          axis.ticks.y = element_line(color = "black", size = 0.5, linetype = 1))
 
-  return(plot)
+  return(p)
 }
