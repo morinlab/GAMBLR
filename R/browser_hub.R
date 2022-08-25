e75928eec4c07dc240910b3fb6f93f58d413f15a
Ryan Morin <rdmorin@sfu.ca>
2022-08-25 08:36:26 -0700

    updates to browser

--- R/browser_hub.R
+++ R/browser_hub.R
@@ -1,4 +1,61 @@
 
+deblacklist_regions = function(regions_bed,projection="grch37"){
+  if(projection=="grch37"){
+    blacklist_file = "/Users/rmorin/git/LLMPP/resources/reference/encode/hg19-blacklist.v2.bed"
+  }
+  blacklist_df = read_tsv(blacklist_file,col_names = c("chr","start","end","name"))
+  blacklist_dt = as.data.table(blacklist_df)
+  colnames(regions_bed)[c(1:3)]=c("chr","start","end")
+  if(any(!grepl("chr",regions_bed[,1]))){
+    message("adding chr prefix")
+    regions_bed = mutate(regions_bed,chr = as.character(chr))
+    regions_bed = mutate(regions_bed,chr = paste0("chr",chr))
+  }
+  regions_dt = as.data.table(regions_bed)
+
+ 
+  #annotate bins that overlap blacklisted regions
+  setkey(blacklist_dt, chr,start,end)
+  setkey(regions_dt, chr,start,end)
+
+  annotated = foverlaps(blacklist_dt, regions_dt,by.x =c("chr","start","end"),by.y=c("chr","start","end"),mult = "all",type = "any") %>% dplyr::filter(!is.na(start))
+  to_keep = dplyr::filter(regions_bed,!start %in% annotated$start)
+  return(to_keep)
+}
+
+load_combine_cached_bins = function(function_params=list(bin_size=15000),
+  additional_details=list(foreground="DLBCL_FL",background="CLL_MM")){
+
+      cache_file_name = paste0(config::get("repo_base"),"cached_results/get_ssm_by_region")
+      for (param in names(function_params)[order(names(function_params))]){
+        cache_file_name = paste0(cache_file_name,"--",param,"-",function_params[[param]])
+      }
+      cache_file_name = paste0(cache_file_name,"*")
+      for (detail in names(additional_details)){
+        cache_file_name = paste0(cache_file_name,"--",detail,"-",additional_details[[detail]])
+      }
+      cache_file_name = paste0(cache_file_name,".tsv")
+     
+    cached = Sys.glob(paths = cache_file_name )
+  if(!length(cached)==48){
+    stop(paste("error: cannot find 48 files using this pattern",cache_file_name))
+  }
+  
+}
+
+bins_to_bedgraph = function(bin_df,min_value = 3,filename = "test.bed"){
+  bed_cols = dplyr::select(bin_df,1,2,3,smoothed_ratio)
+  colnames(bed_cols) = c("chr","start","end","value")
+  if(any(!grepl("chr",bed_cols[,1]))){
+    message("adding chr prefix")
+    bed_cols = mutate(bed_cols,chr = paste0("chr",chr))
+  }
+  bed_cols = dplyr::filter(bed_cols,value > min_value)
+  this = dplyr::filter(chr1p,bin_start==27985000) 
+  bed_cols = mutate(bed_cols,end=format(end, scientific=F),start=format(start, scientific=F))
+  write.table(bed_cols,row.names=F,col.names=F,quote=F,sep="\t",file=filename)
+  
+}
 
 build_browser_ashm = function(browser_base_path="/Users/rmorin/git/LLMPP/",
                               projection="grch37",
