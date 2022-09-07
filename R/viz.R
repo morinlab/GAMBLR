5600ac05a7077fa90071ee71062c45b42b41a0ab
Adam <cmattsson@bcgsc.ca>
2022-09-07 13:30:01 -0700

    fancy_circosplot updates

--- R/viz.R
+++ R/viz.R
@@ -4365,19 +4365,22 @@ comp_report = function(this_sample,
 }
 
 
-#' Create a circos plots visualizing SVS or SSM with gene annotations. 
+#' Create a circos plots visualizing SVS and SSM with optional gene annotations. 
 #'
 #' @param this_sample Sample to be plotted.
 #' @param gene_list Optional parameter to annotate genes on the circos plot from a list of genes (df). Is compatible with gene_to_region (return_as = "bed") output format. See examples.
+#' @param ssm_calls Boolean parameter for plotting ssm. Default is TRUE.
+#' @param sv_calls Boolean parameter for plotting SVs, default is TRUE.
 #' @param chr_select Optional argument for subset on selected chromosomes, default is all autosomes.
 #' @param vaf_cutoff Threshold for filtering variants on VAF (events with a VAF > cutoff will be retained).
-#' @param use_sv_calls Set to TRUE for get_combined_sv approach of retrieving plot data. Set to FALSE to plot SSM with assign_cn_to_ssm. Default is TRUE.
 #' @param coding_only Optional. Set to TRUE to restrict to plotting only coding mutations.
 #' @param from_flatfile If set to TRUE the function will use flat files instead of the database.
 #' @param use_augmented_maf Boolean statement if to use augmented maf, default is TRUE.
 #' @param projection Genomic projection for variants and circos plot. Accepted values are grch37 and hg38, default is grch37.
 #' @param out Path to output folder, to where the plot will be exported.
-#' @param file_name Optional parameter for specifying the file name of generated circos plot, default is "{this_sample}_circos.pdf".
+#' @param plot_title Optional parameter for naming your plot, default is this_sample.
+#' @param pdf Set to FALSE for png, default is TRUE (pdf).
+#' @param file_name Optional parameter for specifying the file name of generated circos plot, default is "{this_sample}_circos.pdf". If pdf is set to FALSE, a png will be generated, thus the .png extension needs to be attached to the file_name.
 #'
 #' @return Nothing.
 #' @import tidyverse RCircos
@@ -4392,58 +4395,88 @@ comp_report = function(this_sample,
 #' # get regions for selected genes
 #' fl_genes_list = gene_to_region(gene_symbol = fl_genes, return_as = "bed")
 #'
-#' #construct fancy circos plot with gene annotations
-#' fancy_circos_plot(this_sample = "HTMCP-01-06-00422-01A-01D",
-#'                   gene_list = fl_genes_list,
-#'                   use_sv_calls = TRUE,
-#'                   projection = "grch37",
-#'                   out = "../../plots/", 
-#'                   file_name = "circos_sv.pdf")
+#' fancy_circos_plot_new(this_sample = "DOHH-2",
+#'                       ssm_calls = FALSE,
+#'                       sv_calls = TRUE,
+#'                       gene_list = fl_genes_list,
+#'                       chr_select = c("chr8", "chr14", "chr18"),
+#'                       coding_only = FALSE,
+#'                       projection = "grch37",
+#'                       out = "../../plots/", 
+#'                       plot_title = "DOHH-2 (SVs) Example Plot",
+#'                       pdf = FALSE, 
+#'                       file_name = "dohh2_example.png")
 #'
 fancy_circos_plot = function(this_sample,
                              gene_list,
+                             ssm_calls = TRUE,
+                             sv_calls = TRUE,
                              chr_select = paste0("chr", c(1:22)),
                              vaf_cutoff = 0,
-                             use_sv_calls = TRUE,
                              coding_only = FALSE,
                              from_flatfile = TRUE,
                              use_augmented_maf = TRUE,
                              projection = "grch37",
+                             plot_title = paste0(this_sample),
                              out,
+                             pdf = TRUE,
                              file_name = paste0(this_sample, "_circos.pdf")){
 
-  #set plotting values based on parameters used
-  if(use_sv_calls && !missing(gene_list)){
+  #set track properties based on selected plotting data
+  if(ssm_calls && sv_calls && !missing(gene_list)){
     gene_con_track = 1
     gene_name_track = 2
-    del_track = 3
-    dup_track = 4
-    trans_track = 5
-    n_track = 5
-  }else if(use_sv_calls && missing(gene_list)){
-    del_track = 1
-    dup_track = 2
+    sv_del_track = 4
+    sv_dup_track = 5
+    ssm_snp_track = 6
+    ssm_dnp_track = 7
+    ssm_del_track = 8
+    ssm_ins_track = 9
+    trans_track = 10
+  }
+  
+  if(ssm_calls && sv_calls && missing(gene_list)){
+    sv_del_track = 1
+    sv_dup_track = 2
+    ssm_snp_track = 3
+    ssm_dnp_track = 4
+    ssm_del_track = 5
+    ssm_ins_track = 6
+    trans_track = 7
+  }
+
+  if(ssm_calls && !sv_calls && !missing(gene_list)){
+    gene_con_track = 1
+    gene_name_track = 2
+    ssm_snp_track = 4
+    ssm_dnp_track = 5
+    ssm_del_track = 6
+    ssm_ins_track = 7
+  }
+
+  if(ssm_calls && !sv_calls && missing(gene_list)){
+    ssm_snp_track = 1
+    ssm_dnp_track = 2
+    ssm_del_track = 3
+    ssm_ins_track = 4
+  }
+
+  if(!ssm_calls && sv_calls && !missing(gene_list)){
+    gene_con_track = 1
+    gene_name_track = 2
+    sv_del_track = 4
+    sv_dup_track = 5
+    trans_track = 6
+  }
+
+  if(!ssm_calls && sv_calls && missing(gene_list)){
+    sv_del_track = 1
+    sv_dup_track = 2
     trans_track = 3
-    n_track = 3
-  }else if(!use_sv_calls && !missing(gene_list)){
-    gene_con_track = 1
-    gene_name_track = 2
-    del_track = 3
-    ins_track = 4
-    snp_track = 5
-    dnp_track = 6
-    n_track = 6
-   }else if(!use_sv_calls && missing(gene_list)){
-    del_track = 1
-    ins_track = 2
-    snp_track = 3
-    dnp_track = 4
-    n_track = 4
-   }
+  }
 
   #sanity checking incoming gene list and renaming columns if needed
   if(!missing(gene_list)){
-
     #check type of incoming gene_list
     if(!is.list(gene_list)){
       message("Please ensure that incoming gene list is in fact a data frame with the following columns: chr:start:end:gene")
@@ -4472,8 +4505,28 @@ fancy_circos_plot = function(this_sample,
     gene_list = gene_list[gene_list$Chromosome %in% chr_select, ]
   }
   
-  if(use_sv_calls){
-    #get variants
+  #get SSM data
+  if(ssm_calls){
+    maf = assign_cn_to_ssm(this_sample = this_sample, coding_only = coding_only, from_flatfile = from_flatfile, use_augmented_maf = use_augmented_maf)$maf #get maf data
+    maf_tmp = dplyr::select(maf, Chromosome, Start_Position, End_Position, Variant_Type) #select appropriate columns
+    maf_tmp$Variant_Size = maf_tmp$End_Position - maf_tmp$Start_Position # calcualte variant size
+    maf_tmp$Variant_Type = as.factor(maf_tmp$Variant_Type) #transform Variant_Type to factor
+    maf_tmp[maf_tmp==0] <- 1 #transform all lenght coordinates == 0 to 1
+
+    if(!str_detect(maf_tmp$Chromosome, "chr")){ #add chr prefic, if missing...
+      maf_tmp = mutate(maf_tmp, Chromosome = paste0("chr", Chromosome))
+    }
+
+    maf_tmp = maf_tmp[maf_tmp$Chromosome %in% chr_select, ] #filter incoming maf on selected chromosomes
+    ssm_del = dplyr::filter(maf_tmp, Variant_Type == "DEL") #subset on deletions
+    ssm_ins = dplyr::filter(maf_tmp, Variant_Type == "INS") #subset on insertions
+    ssm_snp = dplyr::filter(maf_tmp, Variant_Type == "SNP") #subset on single nucleotide polymorphism
+    ssm_dnp = dplyr::filter(maf_tmp, Variant_Type == "DNP") #subset on dinucleotide polymorphism
+    message(paste0(nrow(ssm_del) + nrow(ssm_dnp) + nrow(ssm_ins) + nrow(ssm_snp)), " SSMs found for ", this_sample)
+  }
+
+  #get SVs
+  if(sv_calls){
     svs = get_combined_sv(sample_ids = this_sample, projection = projection)
 
     #filter on vaf
@@ -4488,14 +4541,14 @@ fancy_circos_plot = function(this_sample,
     #rename variables
     colnames(svs_df)[1:7] = c("CHROM_A", "START_A", "END_A", "CHROM_B", "START_B", "END_B", "TYPE")
 
-    #add chr prefix, if missing
+      #add chr prefix, if missing
     if(!str_detect(svs_df$CHROM_A, "chr")[1]){
       svs_df = mutate(svs_df, CHROM_A = paste0("chr", CHROM_A))
-    }
+      }
 
     if(!str_detect(svs_df$CHROM_B, "chr")[4]){
       svs_df = mutate(svs_df, CHROM_B = paste0("chr", CHROM_B))
-    }
+      }
 
     #subset on selected chromosomes
     svs_df = svs_df[svs_df$CHROM_A %in% chr_select, ]
@@ -4515,23 +4568,7 @@ fancy_circos_plot = function(this_sample,
     sv_del$SIZE = sv_del$END_A - sv_del$START_A
     sv_dup$SIZE = sv_dup$END_A - sv_dup$START_A
 
-  }else{
-    maf = assign_cn_to_ssm(this_sample = this_sample, coding_only = coding_only, from_flatfile = from_flatfile, use_augmented_maf = use_augmented_maf)$maf #get maf data
-    maf_tmp = dplyr::select(maf, Chromosome, Start_Position, End_Position, Variant_Type) #select appropriate columns
-    maf_tmp$Variant_Size = maf_tmp$End_Position - maf_tmp$Start_Position # calcualte variant size
-    maf_tmp$Variant_Type = as.factor(maf_tmp$Variant_Type) #transform Variant_Type to factor
-    maf_tmp[maf_tmp==0] <- 1 #transform all lenght coordinates == 0 to 1
-
-    if(!str_detect(maf_tmp$Chromosome, "chr")){ #add chr prefic, if missing...
-      maf_tmp = mutate(maf_tmp, Chromosome = paste0("chr", Chromosome))
-    }
-
-    maf_tmp = maf_tmp[maf_tmp$Chromosome %in% chr_select, ] #filter incoming maf on selected chromosomes
-
-    ssm_del = dplyr::filter(maf_tmp, Variant_Type == "DEL") #subset on deletions
-    ssm_ins = dplyr::filter(maf_tmp, Variant_Type == "INS") #subset on insertions
-    ssm_snp = dplyr::filter(maf_tmp, Variant_Type == "SNP") #subset on single nucleotide polymorphism
-    ssm_dnp = dplyr::filter(maf_tmp, Variant_Type == "DNP") #subset on dinucleotide polymorphism
+    message(paste0(nrow(sv_trans) + nrow(sv_del) + nrow(sv_dup)), " SVs found for ", this_sample)
   }
 
   #plotting
@@ -4549,14 +4586,20 @@ fancy_circos_plot = function(this_sample,
   chr_exclude = setdiff(chr_all, chr_select)
 
   #set core components
-  RCircos.Set.Core.Components(cyto.info = cytobands, chr.exclude = chr_exclude, tracks.inside = n_track, tracks.outside = 0)
+  suppressMessages(RCircos::RCircos.Set.Core.Components(cyto.info = cytobands, chr.exclude = chr_exclude, tracks.inside = 10))
 
   #set plot parameters
   RCircos.params = RCircos.Get.Plot.Parameters()
 
   #define plotting parameters
   out.file = paste0(out, file_name)
-  pdf(out.file, height = 7, width = 7)
+  
+  if(pdf){
+    pdf(out.file, height = 7, width = 7) 
+  }else{
+    png(out.file, height = 7, width = 7, units = "in", res = 300) 
+  }
+  
   RCircos.Set.Plot.Area(margins = 0);
 
   #create empty plot
@@ -4569,52 +4612,68 @@ fancy_circos_plot = function(this_sample,
     RCircos.Gene.Name.Plot(gene_list, 4, gene_name_track, "in")
   }
 
-  #arrange tracks
-  if(use_sv_calls){
-    #translocations
-    RCircos.Ribbon.Plot(sv_trans, track.num = trans_track, by.chromosome = FALSE, twist = TRUE)
-
-    #duplications
-    RCircos.params$track.background = "sienna2"
-    RCircos.params$max.layers = 1
-    RCircos.Reset.Plot.Parameters(RCircos.params)
-    RCircos.Tile.Plot(sv_dup, track.num = dup_track, side = "in")
-
-    #deletions
-    RCircos.params$track.background = "steelblue2"
-    RCircos.params$max.layers = 1
-    RCircos.Reset.Plot.Parameters(RCircos.params)
-    RCircos.Tile.Plot(sv_del, track.num = del_track, side = "in")
-  }
-
-  if(!use_sv_calls){
-    #ssm insertions
-    RCircos.params$track.background = "sienna2"
-    RCircos.params$max.layers = 1
-    RCircos.Reset.Plot.Parameters(RCircos.params)
-    RCircos.Tile.Plot(ssm_ins, track.num = ins_track, side = "in")
-
+  #aadd tracks
+  if(ssm_calls){
     #ssm deletions
     RCircos.params$track.background = "steelblue2"
     RCircos.params$max.layers = 1
     RCircos.Reset.Plot.Parameters(RCircos.params)
-    RCircos.Tile.Plot(ssm_del, track.num = del_track, side = "in")
+    RCircos.Tile.Plot(ssm_del, track.num = ssm_del_track, side = "in")
+
+    #ssm insertions
+    RCircos.params$track.background = "sienna2"
+    RCircos.params$max.layers = 1
+    RCircos.Reset.Plot.Parameters(RCircos.params)
+    RCircos.Tile.Plot(ssm_ins, track.num = ssm_ins_track, side = "in")
 
     #ssm snp
     RCircos.params$track.background = "seagreen"
     RCircos.params$max.layers = 1
     RCircos.Reset.Plot.Parameters(RCircos.params)
-    RCircos.Tile.Plot(ssm_snp, track.num = snp_track, side = "in")
+    RCircos.Tile.Plot(ssm_snp, track.num = ssm_snp_track, side = "in")
 
     #ssm dnp
     RCircos.params$track.background = "tomato4"
     RCircos.params$max.layers = 1
     RCircos.Reset.Plot.Parameters(RCircos.params)
-    RCircos.Tile.Plot(ssm_dnp, track.num = dnp_track, side = "in")
-    }
+    RCircos.Tile.Plot(ssm_dnp, track.num = ssm_dnp_track, side = "in")
+  }
 
-  #close connection
-  dev.off()
+  if(sv_calls){
+    #translocations
+    RCircos.Link.Plot(sv_trans, track.num = trans_track, by.chromosome = FALSE)
+
+    #duplications
+    RCircos.params$track.background = "sienna2"
+    RCircos.params$max.layers = 1
+    RCircos.Reset.Plot.Parameters(RCircos.params)
+    RCircos.Tile.Plot(sv_dup, track.num = sv_dup_track, side = "in")
+
+    #deletions
+    RCircos.params$track.background = "steelblue2"
+    RCircos.params$max.layers = 1
+    RCircos.Reset.Plot.Parameters(RCircos.params)
+    RCircos.Tile.Plot(sv_del, track.num = sv_del_track, side = "in")
+  }
+
+  #add plot title and legends
+  if(sv_calls && !ssm_calls){
+    text(x = 0, y = 2.5, plot_title, font = 2, cex = 1.2)
+    legend(x = 2, y = -1.2, legend = c("Del", "Dup"), bty = "n", col = c("steelblue2", "sienna2"), pch = 19, text.col = "black", horiz = FALSE, pt.cex = 2, title = "SVs", inset = c(0.1, 0.1)) 
+  }
+
+  if(!sv_calls && ssm_calls){
+    text(x = 0, y = 2.5, plot_title, font = 2, cex = 1.2)
+    legend(x = 2, y = -1.8, legend = c("SNP", "DNP", "Del", "Ins"), bty = "n", col = c("seagreen", "tomato4", "steelblue2", "sienna2"), pch = 19, text.col = "black", horiz = FALSE, pt.cex = 2, title = "SSM", inset = c(0.1, 0.1)) 
+  }
+
+  if(sv_calls && ssm_calls){
+    text(x = 0, y = 2.5, plot_title, font = 2, cex = 1.2)
+    legend(x = 2, y = -1.2, legend = c("Del", "Dup"), bty = "n", col = c("steelblue2", "sienna2"), pch = 19, text.col = "black", horiz = FALSE, pt.cex = 2, title = "SVs", inset = c(0.1, 0.1)) 
+    legend(x = 2, y = -1.8, legend = c("SNP", "DNP", "Del", "Ins"), bty = "n", col = c("seagreen", "tomato4", "steelblue2", "sienna2"), pch = 19, text.col = "black", horiz = FALSE, pt.cex = 2, title = "SSM", inset = c(0.1, 0.1)) 
+  }
+
+  invisible(dev.off())
 }
 
 
