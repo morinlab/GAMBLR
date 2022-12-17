42c107513ebe8e2466aae3ae0043cda4043da89f
Kdreval <k.dreval@gmail.com>
2022-12-16 17:51:21 -0800

    cleaup: shift chapuy function to helper

--- R/utilities.R
+++ R/utilities.R
@@ -4834,3 +4834,327 @@ classify_dlbcl_chapuy <- function(
     }
 
 }
+
+
+
+#' Classify DLBCLs according to genetic subgroups of Chapuy et al.
+#'
+#' Use the feature weights from NMF model to assemble the binary matrix and classify DLBCL tumors based on C0-C5 system of Chapuy et al
+#'
+#' @param these_samples_metadata The metadata data frame that contains sample_id column with ids for the samples to be classified.
+#' @param maf_data The MAF data frame to be used for matrix assembling. At least must contain the first 45 columns of standard MAF format.
+#' @param seg_data The SEG data frame to be used for matrix assembling. Must be of standard SEG formatting, for example, as returned by get_sample_cn_segments.
+#' @param sv_data The SV data frame to be used for matrix assembling. Must be of standard BEDPE formatting, for example, as returned by get_combined_sv.
+#' @param projection The projection of the samples. Only used to retrerive data through GAMBLR when it is not provided. Defaults to grch37.
+#' @param output The output to be returned after prediction is done. Can be one of predictoins, matrix, or both. Defaults to predictions.
+#' @return data frame with classification, binary matrix used in classification, or both
+#' @import data.table circlize tidyverse
+#'
+classify_dlbcl_chapuy <- function(
+    these_samples_metadata,
+    maf_data,
+    seg_data,
+    sv_data,
+    projection = "grch37",
+    output = "predictions"
+){
+    # Assembling the feature matrix based on the guidance
+    # non-synonymous mutations, 2; synonymous mutations, 1; no-mutation, 0;
+    # high-grade CN gain [CN ≥ 3.7 copies], 2; low-grade CN gain [3.7 copies ≥ CN ≥ 2.2 copies], 1;
+    # CN neutral, 0;
+    # low-grade CN loss [1.1 ≤ CN ≤1.6 copies], 1; high-grade CN loss [CN ≤ 1.1 copies], 2;
+    # chromosomal rearrangement present, 3; chromosomal rearrangement absent, 0
+    chapuy_feature_matrix <- list()
+
+    # Mutations matrix
+    chapuy_feature_matrix$ssm_matrix <- maf_data %>%
+        dplyr::filter(
+          Hugo_Symbol %in% chapuy_features$ssm_features
+        ) %>%
+        dplyr::filter(
+          Variant_Classification %in% c(
+            "Silent",
+            GAMBLR:::coding_class
+        )) %>%
+        dplyr::select(
+            Tumor_Sample_Barcode,
+            Hugo_Symbol,
+            Variant_Classification
+        ) %>%
+        dplyr::mutate(
+            mutated = ifelse(
+                Variant_Classification == "Silent",
+                1,
+                2
+            )
+        ) %>%
+        dplyr::select(-Variant_Classification) %>%
+        group_by(Tumor_Sample_Barcode,Hugo_Symbol) %>%
+        dplyr::arrange(Tumor_Sample_Barcode, desc(mutated)) %>%
+        dplyr::filter( # if both syn and nonsyn are present, prioritize nonsyn
+          mutated==max(mutated)
+        ) %>%
+        slice_head %>%
+        ungroup %>%
+        pivot_wider(
+            names_from = "Hugo_Symbol",
+            values_from = "mutated"
+        ) %>%
+        replace(is.na(.), 0) %>%
+        column_to_rownames("Tumor_Sample_Barcode")
+
+    chapuy_feature_matrix$ssm_matrix <- complete_missing_from_matrix(
+        chapuy_feature_matrix$ssm_matrix,
+        these_samples_metadata$sample_id
+    )
+
+    # CNV matrix
+    if(projection=="grch37"){
+        arm_coordinates <- GAMBLR::chromosome_arms_grch37
+        cytoband_coordinates <- circlize::read.cytoband(species = "hg19")$df %>%
+            `names<-`(c("chr", "start", "end", "cytoband", "extra")) %>%
+            dplyr::mutate(chr = gsub("chr", "", chr)) %>%
+            dplyr::mutate(cytoband=paste0(chr,cytoband)) %>%
+            dplyr::select(-extra)
+    }else{
+        arm_coordinates <- GAMBLR::chromosome_arms_hg38
+        cytoband_coordinates <- circlize::read.cytoband(species = "hg38")$df %>%
+            `names<-`(c("chr", "start", "end", "cytoband", "extra")) %>%
+            dplyr::mutate(cytoband=paste0(chr,cytoband)) %>%
+            dplyr::select(-extra)
+    }
+
+    # First the arm features
+    cnv_features_arm <- arm_coordinates %>%
+        mutate(arm = paste0(
+                chromosome,
+                arm)
+            ) %>%
+        left_join(
+                chapuy_features$cnv_features_arm,
+                .,
+                by="arm"
+            ) %>%
+        as.data.table %>%
+        setkey(chromosome, start, end)
+
+    # Next, the cytoband features
+    cnv_features_cytoband <- cytoband_coordinates %>%
+        left_join(
+                chapuy_features$cnv_features_cytoband,
+                .,
+                by="cytoband"
+            ) %>%
+        as.data.table %>%
+        setkey(chr, start, end)
+
+    cnv_arms <- foverlaps(
+          seg_data,
+          cnv_features_arm,
+          nomatch = 0
+        ) %>%
+        dplyr::select(sample, arm, CNV, log.ratio) %>%
+        dplyr::rename("feature"="arm")
+
+    cnv_cytobands <-  foverlaps(
+          seg_data,
+          cnv_features_cytoband,
+          nomatch = 0
+        ) %>%
+        select(sample, cytoband, CNV, log.ratio) %>%
+        rename("feature"="cytoband")
+
+    chapuy_feature_matrix$cnv_matrix <- bind_rows(
+          cnv_arms,
+          cnv_cytobands
+        ) %>%
+        group_by(sample, feature, CNV) %>%
+        summarise(
+          featuremean = mean(log.ratio)
+        ) %>%
+        # get rid of neutrals
+        dplyr::filter(
+          !featuremean == 0
+        ) %>%
+        # ensure the same direction
+        dplyr::filter(
+          (featuremean>0 & CNV=="AMP") | (featuremean<0 & CNV=="DEL")
+        ) %>%
+        dplyr::mutate(
+          CN = 2*2^featuremean,
+          mutated = case_when(
+            CNV=="AMP" & CN >=3.7 ~ 2,
+            CNV=="AMP" & CN >=2.2 ~ 1,
+            CN > 1.6 ~ 0,
+            CNV=="DEL" & CN >1.1 ~ 1,
+            CNV=="DEL" & CN <=1.1 ~ 2
+            ),
+          featurename = paste0(feature,":",CNV)
+        ) %>%
+        ungroup %>%
+        dplyr::select(sample, mutated, featurename) %>%
+        pivot_wider(
+            names_from = "featurename",
+            values_from = "mutated"
+        ) %>%
+        replace(is.na(.), 0) %>%
+        column_to_rownames("sample")
+
+    chapuy_feature_matrix$cnv_matrix <- complete_missing_from_matrix(
+        chapuy_feature_matrix$cnv_matrix,
+        these_samples_metadata$sample_id
+    )
+
+    # SV matrix
+    chapuy_feature_matrix$sv_matrix <- sv_data %>%
+        dplyr::filter(
+          gene %in% chapuy_features$sv_features |
+          partner %in% chapuy_features$sv_features
+        ) %>%
+        dplyr::mutate(
+          feature = case_when(
+            gene %in% chapuy_features$sv_features ~ paste0("SV:",gene),
+            partner %in% chapuy_features$sv_features ~ paste0("SV:",partner)
+        )) %>%
+        dplyr::mutate(
+          mutated=3
+        ) %>%
+        distinct(
+          tumour_sample_id, feature, mutated
+        ) %>%
+        ungroup %>%
+        pivot_wider(
+            names_from = "feature",
+            values_from = "mutated"
+        ) %>%
+        replace(is.na(.), 0) %>%
+        column_to_rownames("tumour_sample_id")
+
+    chapuy_feature_matrix$sv_matrix <- complete_missing_from_matrix(
+        chapuy_feature_matrix$sv_matrix,
+        these_samples_metadata$sample_id
+    )
+
+    if("SV:CD274" %in% colnames(chapuy_feature_matrix$sv_matrix)){
+      chapuy_feature_matrix$sv_matrix <- chapuy_feature_matrix$sv_matrix %>%
+        dplyr::rename("SV:CD274/PDCD1LG2" = "SV:CD274")
+    }
+
+
+    # Generate complete matrix
+    chapuy_feature_matrix$complete_matrix <- bind_cols(
+        chapuy_feature_matrix$ssm_matrix,
+        chapuy_feature_matrix$cnv_matrix,
+        chapuy_feature_matrix$sv_matrix
+    ) %>% as.data.frame
+
+    # Check if any features are missing
+    missing_features <- setdiff(
+        chapuy_features$feature_weights$Feature,
+        colnames(chapuy_feature_matrix$complete_matrix)
+    )
+
+    if(length(missing_features)>0){
+        message(
+            "ATTENTION: Not all features are available in the data!"
+        )
+        message(
+            paste0(
+                "A total of ",
+                length(
+                   missing_features
+                ),
+                " features are missing:"
+            )
+        )
+        message(
+            paste(
+                missing_features,
+                collapse=", "
+            )
+        )
+        message(
+            "They will be set to 0, which may affect model performance."
+        )
+        chapuy_feature_matrix$complete_matrix[,missing_features] <- 0
+    }
+
+    # This is to ensure consistent ordering for a fool-proof downstream calculations
+    chapuy_feature_matrix$complete_matrix <- chapuy_feature_matrix$complete_matrix %>%
+      dplyr::select(chapuy_features$feature_weights$Feature)
+
+    # If user only wants matrix, return it here and do not perform the
+    # subsequent analysis
+    if(output=="matrix"){
+      return(chapuy_feature_matrix$complete_matrix)
+    }
+
+    # Classify the samples
+    message("Assembled the matrix, classifying the samples ...")
+    features_weights_matrix <- chapuy_features$feature_weights %>%
+      column_to_rownames("Feature") %>%
+      as.data.frame
+
+    compute_cluster_probability <- function(Row) {
+      ((Row %>% t) * features_weights_matrix) %>%
+      colSums %>%
+      as.data.frame %>%
+      `names<-`(
+        rownames(Row)
+      )
+    }
+
+    predictions <- apply(
+      chapuy_feature_matrix$complete_matrix,
+      1,
+      compute_cluster_probability
+    )
+
+    predictions <- do.call(
+      cbind,
+      predictions
+    ) %>%
+    as.data.frame %>%
+    t %>% # The output is wide so convert it to have 1 row/sample
+    as.data.frame
+
+    # Layer in which cluster the sample belongs to
+    # by taking the highest sum of weights
+    predictions$predict <- colnames(predictions)[apply(predictions,1,which.max)]
+
+    predictions <- predictions %>%
+      rownames_to_column("sample_id")
+
+    # Account for C0 samples, which will have all weights calculated as 0
+    predictions <- predictions %>%
+      rowwise() %>%
+      dplyr::mutate(
+        predict = ifelse(
+          sum(C1:C5)==0,
+          "C0",
+          predict
+      )) %>%
+      ungroup %>%
+      as.data.frame
+
+    if(output == "predictions"){
+      return(predictions)
+    }else if (output == "both") {
+      return(
+        list(
+          matrix = chapuy_feature_matrix$complete_matrix,
+          predictons = predictions
+        )
+      )
+    }else{
+      stop(
+        paste0(
+          "You requested to return ",
+          output,
+          ", which is not supported.\n",
+          "Please specify one of matrix, predictions, or both."
+        )
+      )
+    }
+
+}
