f044eedc5c29b0bb25a4c6e74f61730c8ec7fcf0
Houman <houmanmirhoseini@yahoo.com>
2023-11-28 09:24:34 -0800

    annotate_maf_triplet update

--- R/annotate_maf_triplet.R
+++ R/annotate_maf_triplet.R
@@ -1,19 +1,21 @@
 #' @title Annotate MAF with triplet context
 #'
-#' @description Give triple sequence of mutated base with its adjacent bases (-1 and +1) based on the STRAND_VEP column of MAF 
+#' @description Give triple sequence of mutated base with its adjacent bases (-1 and +1)  
 #'
 #' @details It gives the reference and alternative alleles and looks for the rows of the data frame based on these values for + strand genes and their 
 #' complement alleles rows for - strand genes, then it can look for the adjacent bases in that mutation position. Also, it can look for all the SNVs in 
-#' the MAF data frame and provide triple sequences for them.
+#' the MAF data frame and provide triple sequences for them (reverse complement sequence for the - strand). 
 #'
-#' @param maf MAF file (required columns: Reference_Allele, Tumor_Seq_Allele2, STRAND_VEP)
+#' @param maf MAF file (required columns: Reference_Allele, Tumor_Seq_Allele2)
 #' @param all_SNVs To give us all the triplet sequences of SNVs and not specifying any specific ref and alt alleles (default is TRUE)
 #' @param ref Reference allele
 #' @param alt Alternative allele
 #' @param projection The genome build projection for the variants you are working with (default is grch37)
 #' @param fastaPath Can be a path to a FASTA file
+#' @param bsgenome_name Name of a BSgenome data package (It has 4 or 5 parts, separated by dot: 1st(BSgenome) . 2nd:name of organism(Hsapiens) . 3rd:name of genome provider (UCSC, NCBI, TAIR,...) . 4th:name of NCBI assembly (e.g. GRCh38) or UCSC genome (e.g. hg38) . 5th(optional): If the package contains masked sequences (masked)) 
+#' @param pyrimidine_collapse Estimate mutation_strand and 
 #'
-#' @return A data frame with an extra column for triple sequence
+#' @return A data frame with two to three extra columns, in case pyrimidine_collapse = FALSE, it will add triple sequence (seq) and the strand (mutation_strand). When pyrimidine_collapse = T, another column also will be added to the those extra columns which shows the mutation (mutation)
 #'
 #' @import Rsamtools dplyr BSgenome
 #' @export
@@ -84,17 +86,29 @@ annotate_maf_triplet = function(maf,
     'G'= 'C'
   )  
   if(pyrimidine_collapse){
-    maf = mutate(maf,mutation_strand=ifelse(Reference_Allele %in% c("A","G"),"-","+")) %>%
-      mutate(mutation = ifelse(Reference_Allele %in% c("A","G"),
-                               paste0(complement[Reference_Allele],">",complement[Tumor_Seq_Allele2]),
-             paste0(Reference_Allele,">",Tumor_Seq_Allele2)))
+    maf <- maf %>%
+        dplyr::mutate(
+            mutation_strand=ifelse(
+                Reference_Allele %in% c("A","G"),
+                    "-",
+                        "+"
+            )
+        ) %>%
+        dplyr::mutate(
+            mutation = ifelse(
+                Reference_Allele %in% c("A","G"),
+                     paste0(complement[Reference_Allele],">",complement[Tumor_Seq_Allele2]),
+                        paste0(Reference_Allele,">",Tumor_Seq_Allele2)))
   }else{
-    maf = mutate(maf,mutation_strand="+")
+    maf = dplyr::mutate(
+        maf,
+        mutation_strand="+")
   }
   
   CompRef = complement[ref]
   CompAlt = complement[alt]
   # Provide triple sequence for all the SNVs
+    # Using BSgenome
   if (all_SNVs == TRUE){
     if(bsgenome_loaded){
       sequences <- maf %>%
@@ -103,35 +117,39 @@ annotate_maf_triplet = function(maf,
             (nchar(maf$Reference_Allele) == 1 &
                nchar(maf$Tumor_Seq_Allele2) == 1
             ),
-            as.character(getSeq(
-              genome,
-              maf$Chromosome,
-              start = maf$Start_Position-1,
-              end = maf$Start_Position + 1,
-              strand = mutation_strand
-              )),
+            as.character(
+                Rsamtools::getSeq(
+                    genome,
+                    maf$Chromosome,
+                    start = maf$Start_Position-1,
+                    end = maf$Start_Position + 1,
+                    strand = mutation_strand
+                )
+            ),
             "NA"
           )
         )
       
     }else{
-
-      #TODO: if we want this to work with a local fasta we need to ensure the - strand is returned. I couldn't test it on my Laptop
+      # Using local Fasta file
       sequences <- maf %>%
         dplyr::mutate(
           seq = ifelse(
             (nchar(maf$Reference_Allele) == 1 &
                nchar(maf$Tumor_Seq_Allele2) == 1
             ),
-            Rsamtools::getSeq(
-              fasta,
-              GenomicRanges::GRanges(
-                maf$Chromosome,
-                IRanges(
-                  start = maf$Start_Position - 1,
-                  end = maf$Start_Position + 1
+            as.character(
+                Rsamtools::getSeq(
+                    fasta,
+                    GenomicRanges::GRanges(
+                        maf$Chromosome,
+                        IRanges(
+                            start = maf$Start_Position - 1,
+                            end = maf$Start_Position + 1
+                        ),
+                            strand = maf$mutation_strand
+                    )
                 )
-              )
             ),
             "NA"
           )
@@ -143,29 +161,29 @@ annotate_maf_triplet = function(maf,
     # Provide triple sequence of + strand and reverse complement of - strand   
     # Mutations on + strand with chosen ref and alt alleles
     # Mutations on - strand with complement ref and alt alleles
-    if (!"STRAND_VEP" %in% colnames(maf)){
-      stop("STRAND_VEP column is not in the MAF")
-    }
     sequences <- maf %>%
       dplyr::mutate(
         seq = ifelse(
-          (maf$STRAND_VEP == "1" &
+          (maf$mutation_strand == "+" &
              maf$Reference_Allele == ref &
              maf$Tumor_Seq_Allele2 == alt
           )|(
-            maf$STRAND_VEP == "-1" &
+            maf$mutation_strand == "-" &
               maf$Reference_Allele == CompRef &
               maf$Tumor_Seq_Allele2 == CompAlt
           ),
-          Rsamtools::getSeq(
-            fasta,
-            GenomicRanges::GRanges(
-              maf$Chromosome,
-              IRanges(
-                start = maf$Start_Position - 1,
-                end = maf$Start_Position + 1
-              )
-            )
+          as.character(
+              Rsamtools::getSeq(
+                 fasta,
+                 GenomicRanges::GRanges(
+                    maf$Chromosome,
+                    IRanges(
+                        start = maf$Start_Position - 1,
+                        end = maf$Start_Position + 1
+                    ),
+                        strand = maf$mutation_strand
+                 )
+             )
           ),
           "NA"
         )
