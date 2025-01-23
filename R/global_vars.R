93d0ae4b406488909019b739fe7a198b948b4b9e
Kdreval <k.dreval@gmail.com>
2025-01-23 15:12:38 -0800

    revert to master

--- R/global_vars.R
+++
@@ -1,2 +0,0 @@
-#adding coding_vc to global enviroment
-coding_vc = c("Frame_Shift_Del", "Frame_Shift_Ins", "In_Frame_Del", "In_Frame_Ins", "Missense_Mutation", "Nonsense_Mutation", "Nonstop_Mutation", "Splice_Region", "Splice_Site", "Targeted_Region", "Translation_Start_Site")
